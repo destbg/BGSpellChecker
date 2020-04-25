c27c62e11a9bff17ca1204640d8532c6d544f53a
Nikolai <35627380+destbg@users.noreply.github.com>
2020-04-25 19:57:26 +0300

    Revert "bug with path"
    This reverts commit c28dd9d4b2923030162a3dd9b8130662c926da3a.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4200;
 let words;
 
-require('fs').readFile('src/bg-words-cyrillic.txt', 'utf8', (_, data) => {
-  data = data.split('\n').filter((f) => f);
-  data.sort((a, b) => a.length - b.length);
-  words = JSON.stringify(data);
-});
+require('fs').readFile(
+  path.join(__dirname, 'src', 'bg-words-cyrillic.txt'),
+  'utf8',
+  (_, data) => {
+    data = data.split('\n').filter((f) => f);
+    data.sort((a, b) => a.length - b.length);
+    words = JSON.stringify(data);
+  },
+);
 
-app.use(express.static('src'));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/words', (_, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -17,7 +22,7 @@ app.get('/words', (_, res) => {
 });
 
 app.get('/*', (_, res) => {
-  res.sendFile('src/index.html');
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(PORT, () => {
